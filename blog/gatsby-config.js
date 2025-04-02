3d20163e2a40a8b7dc3bce5a91c5c7e1c08ed1d3
unknown <ladinom.gmatc.matc.edu>
2025-04-02 14:31:25 -0500

    env

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -11,10 +11,10 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
     description: ``,
-    author: `Mike Hunsicker`,
+    author: `Michelle Ladino`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
     contact: {
-      name: `Mike Hunsicker`,
+      name: `Michelle Ladino`,
       company: `Blogs Inc.`,
       address: `PO Box 1234`
     }
@@ -23,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `ze0w921s1icg`,
-        accessToken: `bb1122b25d90173402833d33e7e05ab97574fb04c6935acf6e01283ff7467d77`
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
       }
     },
     `gatsby-plugin-image`,
