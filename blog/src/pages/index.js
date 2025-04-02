029fea498ee58deab11b633aef9a1e08c57cc330
ladinom <ladinom@gmatc.matc.edu>
2025-04-02 13:41:25 -0500

    Initialize activity-3

---
+++ blog/src/pages/index.js
@@ -0,0 +1,64 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <ul className={styles.list}>
+      {
+        data.allContentfulBlogPost.edges.map(edge => (
+          <li key={edge.node.id}>
+            <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+              <GatsbyImage
+                image={edge.node.heroImage.gatsbyImageData} 
+                />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
+          </li>
+        ))
+      }
+    </ul>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
+
+export const query = graphql`
+  { 
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
+        }
+      }
+    }
+  }
+`
