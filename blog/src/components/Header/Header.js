cc049980d8b38a5578e2aad197ccc8bf74eda69c
unknown <ladinom.gmatc.matc.edu>
2025-04-09 14:38:31 -0500

    Completed Activity 7

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1 } from "../Heading"
+import { Section } from '../Section'
 
 
 const StyledHeader = styled.header`
@@ -22,11 +23,16 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
+    <Section width={11/12}>
     <StyledLink to="/">
       <H1>
         {siteTitle}
       </H1>
     </StyledLink>
+  </Section>
+  <Section width={1/12}>
+    Search
+  </Section>
   </StyledHeader>
 )
 
