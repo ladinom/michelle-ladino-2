850384f943a901c401edc478ffd7728da2be2480
unknown <ladinom.gmatc.matc.edu>
2025-04-27 17:45:01 -0500

    Buttons

--- blog/src/components/themes/Gray/Gray.js
+++ blog/src/components/themes/Gray/Gray.js
@@ -1,8 +1,50 @@
+import React from 'react'
+import mainHeaderImage from './images/react.png'
+import { Search } from 'styled-icons/feather'
+
+const images = {
+  mainHeaderImage
+}
+
+const icons = {
+  Search: <Search />
+}
+
 const theme = {
-  header: {
-    backgroundColor: `var(--color-primary)`,
-    color: `var(--color-contrast)`
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  colors: {
+    grays: { // from light to dark
+      0: 'hsl(208, 13%, 100%)',
+      1: 'hsl(208, 13%, 95%)',
+      2: 'hsl(208, 13%, 85%)',
+      3: 'hsl(208, 13%, 75%)',
+      4: 'hsl(208, 13%, 65%)',
+      5: 'hsl(208, 13%, 55%)',
+      6: 'hsl(208, 13%, 45%)',
+      7: 'hsl(208, 13%, 35%)',
+      8: 'hsl(208, 13%, 25%)',
+      9: 'hsl(208, 13%, 15%)',
+      10: 'hsl(208, 13%, 5%)'
+    }
   }
 }
 
-export { theme as Gray }
+const variants = {
+  iconButton: {
+    primary: {
+      color: theme.colors.grays[8]
+    },
+    contrast: {
+      color: theme.colors.grays[0]
+    }
+  },
+  header: {
+    primary: {
+      // You could also use color vars from the global styles, e.g., 'var(--color-primary)'
+      backgroundColor: theme.colors.grays[8],
+      color: theme.colors.grays[0]
+    }
+  }
+}
+
+export const Gray = { ...theme, variants, images, icons }
