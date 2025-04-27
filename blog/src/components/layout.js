bd1848e1355d7d337e7ed6f077b3d7cc74f466e0
unknown <ladinom.gmatc.matc.edu>
2025-04-27 18:01:49 -0500

    layout

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -29,13 +29,13 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={Gray} >
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <Content>
-        <Main>{children}</Main>
+        <Main m={20}>{children}</Main>
         <Footer
-          style={{
-            marginTop: `var(--space-S)`,
-            fontSize: `var(--font-sm)`,
-          }}
-          >
+        style={{
+          marginTop: `var(--space-5)`,
+          fontSize: `var(--font-sm)`,
+        }}
+        >
           © {new Date().getFullYear()} &middot; Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
